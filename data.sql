b7cc219cc269566e5e16fbc5f98eb50f77de5465
GalichaUCOS <167277459+GalichaUCOS@users.noreply.github.com>
2024-09-01 17:14:39 +0800

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,1 +1,24 @@
+SELECT * FROM Users
+WHERE DateOfBirth BETWEEN '2000-01-01' AND '2004-12-31';
 
+SELECT * FROM Posts
+WHERE PostedBy = 4;
+
+SELECT GroupName FROM Groups;
+
+SELECT * FROM GroupMembership
+WHERE UserID = 2;
+
+SELECT * FROM Friends
+WHERE UserID1 = 2 OR UserID2 = 2;
+
+SELECT * FROM FriendRequests
+WHERE RequesterID = 1;
+
+SELECT Posts.* 
+FROM Posts 
+JOIN GroupPosts ON Posts.PostID = GroupPosts.PostID
+WHERE GroupPosts.GroupID = 2 AND Posts.IsVisible = TRUE;
+
+SELECT * FROM GroupMembership
+WHERE GroupID = 2 AND IsAccepted = FALSE;
